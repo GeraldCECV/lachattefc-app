95e424f518f9672cfc6fef0204faa50528873f64
Gerald CECV <gerald.cecv@ypocamp.fr>
2026-06-27 14:21:53 +0000

    revert: revenir avant modifications SW

--- public/sw-override.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting())
-self.addEventListener('activate', async () => {
-  const cacheNames = await caches.keys()
-  await Promise.all(cacheNames.map(name => caches.delete(name)))
-  await self.registration.unregister()
-})
--- vite.config.js
+++ vite.config.js
@@ -19,14 +19,22 @@ export default defineConfig({
         start_url: '/',
         scope: '/',
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,ico,png,svg,webmanifest}'],
-        navigateFallback: null,
-        navigateFallbackDenylist: [/^\//],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}']
       }
     })
   ],
