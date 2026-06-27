ed6b93fc47d2b24d586b6635cdc596f4df2aae1f
Gerald CECV <gerald.cecv@ypocamp.fr>
2026-06-27 14:20:41 +0000

    fix: SW navigateFallbackDenylist + sw-override.js

--- vite.config.js
+++ vite.config.js
@@ -19,27 +19,15 @@ export default defineConfig({
         start_url: '/',
         scope: '/',
         icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       },
       workbox: {
-        globPatterns: [],
+        globPatterns: ['**/*.{js,css,ico,png,svg,webmanifest}'],
         navigateFallback: null,
-        runtimeCaching: [],
-      },
-      injectRegister: null,
-      selfDestroying: true,
+        navigateFallbackDenylist: [/^\//],
+      }
     })
   ],
 })
